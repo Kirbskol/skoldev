04ccdbfab807c3537c12c9c412933454a6e85256
Kirbskol <138513974+Kirbskol@users.noreply.github.com>
2025-05-16 19:37:24 +0200

    Add favicon and site title

--- src/constants/urls.jsx
+++ src/constants/urls.jsx
@@ -1,2 +1,3 @@
 export const skolCDN_URL = "https://res.cloudinary.com/kirbskol/image/upload/v1747230251/"
 export const logo_URL = "kirbskol_tbikvc.png"
+export const favicon_URL = "favicon_d1dshj.ico"
