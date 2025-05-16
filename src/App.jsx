f0ae1bbc8daeaa8c3a8b3fab9de839c5cb94c1d4
Kirbskol <138513974+Kirbskol@users.noreply.github.com>
2025-05-16 20:23:38 +0200

    Fix height properties

--- src/App.jsx
+++ src/App.jsx
@@ -15,33 +15,35 @@ function App() {
 
   return (
     <>
-      <div className="flex flex-col items-center">
-        <a href="./" target="_self">
-          <Logo />
-        </a>
-      </div>
-      <h1>Kirbskol</h1>
-      <div className="card">
-        <p>
-          <code>Website under development{'.'.repeat(dotCount)}</code>
-        </p>
-      </div>
-      <div className="flex justify-center gap-4">
-        <a href="https://github.com/Kirbskol" target="_blank">
-          <button className="text-white h-[3em]">
-            <GitHub /> 
-          </button>
-        </a>
-        <a href="mailto:jake@kirbskol.com" target="_blank">
-          <button className="text-white h-[3em]">
-            Contact Me 
-          </button>
-        </a>
-        <a href="https://www.linkedin.com/in/kirbskol/" target="_blank">
-          <button className="text-white h-[3em]">
-            <LinkedIn /> 
-          </button>
-        </a>
+      <div className="flex flex-col items-center justify-center">
+        <div className="flex flex-col items-center">
+          <a href="./" target="_self">
+            <Logo />
+          </a>
+        </div>
+        <h1>Kirbskol</h1>
+        <div className="card">
+          <p>
+            <code>Website under development{'.'.repeat(dotCount)}</code>
+          </p>
+        </div>
+        <div className="flex justify-center gap-4">
+          <a href="https://github.com/Kirbskol" target="_blank">
+            <button className="text-white h-[3em]">
+              <GitHub /> 
+            </button>
+          </a>
+          <a href="mailto:jake@kirbskol.com" target="_blank">
+            <button className="text-white h-[3em]">
+              Contact Me 
+            </button>
+          </a>
+          <a href="https://www.linkedin.com/in/kirbskol/" target="_blank">
+            <button className="text-white h-[3em]">
+              <LinkedIn /> 
+            </button>
+          </a>
+        </div>
       </div>
     </>
   )
